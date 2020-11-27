44993baea39879c3b4cce1dd9382711d4674130a
Mario <vcmario3@gmail.com>
2020-11-27 15:18:05 +0100

    Poder escribir de un color de forma más sencilla

--- Escribir/escribir.cpp
+++ Escribir/escribir.cpp
@@ -12,27 +12,32 @@ void esperar(int tiempo)
     Sleep(tiempo);
 }
 
-void escribir(string mensaje)
+void escribir(string mensaje, unsigned char color = 7)
 {
+    if (color != 7)
+        consolaColor(color);
     for (char &c : mensaje)
     {
         cout << c;
         esperar(TIEMPO_CARACTER);
     }
     cout << endl;
+    consolaColor(7);
     esperar(TIEMPO_LINEA);
 }
 
-void color(int c) {
+void consolaColor(unsigned char c)
+{
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(h, c);
 }
 
-void listaColores() {
-    for(int i = 1; i < 255; i++)
+void listaColores()
+{
+    for (unsigned char i = 1; i < 255; i++)
     {
-        color(i);
-        cout << (i) << " - Prueba de colores" << endl;
+        consolaColor(i);
+        cout << int(i) << " - Prueba de colores" << endl;
     }
-    color(colores::blancoMate);
+    consolaColor(7);
 }
--- Escribir/escribir.h
+++ Escribir/escribir.h
@@ -5,41 +5,7 @@
 using namespace std;
 
 void esperar();
-void escribir(string mensaje);
+void escribir(string mensaje, unsigned char color = 7);
 void listaColores();
-enum colores {
-    azulMate = 1,
-    verdeMate,
-    azulClaroMate,
-    rojoMate,
-    purpuraMate,
-    amarilloOscuro,
-    blancoMate,
-    gris,
-    azul,
-    verde,
-    cyan,
-    salmon,
-    purpura,
-    amarilloClaro,
-    blanco,
-    AO_grisOscuro,
-    AO_azulOscuro,
-    AO_turquesaOscuro,
-    AO_azulClaro,
-    AO_rojo,
-    AO_marron,
-    AO_amarilloOscuro,
-    AO_blancoMate,
-    AO_gris,
-    AO_azul,
-    AO_turquesa,
-    AO_azulBrillante,
-    AO_salmon,
-    AO_purpura,
-    AO_amarilloClaro,
-    AO_blanco,
-    V_negro,
-    V_azulOscuro
-};
+
 #endif // ESCRIBIR_H_INCLUDED
